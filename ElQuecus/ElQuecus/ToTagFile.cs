ac317a316244d48dcfa234db514415b146f73773
Kazauwa <i.heart.d.dead@gmail.com>
2016-02-28 15:22:06 +0300

    Created converter to TagFile

--- ElQuecus/ElQuecus/ToTagFile.cs
+++ ElQuecus/ElQuecus/ToTagFile.cs
@@ -9,6 +9,16 @@ namespace ElQuecus
 {
     public static class ToTagFile
     {
+        public static async Task<List<TagLib.File>> GetFiles(string path)
+        {
+            List<TagLib.File> FileList = new List<File>();
 
+            foreach (var file in await FileSearch.GetFilePaths(path))
+            {
+                FileList.Add(File.Create(file));
+            }
+
+            return FileList;
+        }
     }
 }
