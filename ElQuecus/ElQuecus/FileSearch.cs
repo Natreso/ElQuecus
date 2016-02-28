38e50e77a503df36cc17719b39675afb3ea6176b
Kazauwa <i.heart.d.dead@gmail.com>
2016-02-28 15:05:08 +0300

    Created FileSearch class

---
+++ ElQuecus/ElQuecus/FileSearch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ElQuecus
+{
+    public static class FileSearch
+    {
+        //поле для хранения того что будем возвращать
+        private static List<string> FilePaths = new List<string>();
+
+        private static string[] AudioExtensions = new string[]
+        {
+            ".mp3", ".flac"
+        };
+
+        public static async Task<List<string>> GetFilePaths(string path)
+        {
+            Task t = new Task(() => GetFiles(path));
+            t.Start();
+            await t;
+            return FilePaths;
+        }
+
+        private static void GetFiles(string path)
+        {
+            //берем файл
+            foreach (var directory in Directory.GetDirectories(path))
+            {
+                foreach (var file in Directory.GetFiles(directory))
+                {
+                    if (AudioExtensions.Contains(Path.GetExtension(file)))
+                    FilePaths.Add(file);
+                }
+                //идём по подпапкам
+                GetFiles(directory);
+
+            }
+        }
+    }
+}
