c39f604d339bcc1d65f988632e11e5fc5791bbd0
Kazauwa <i.heart.d.dead@gmail.com>
2016-02-29 23:08:14 +0300

    Fetch SongList in Window Model

--- ElQuecus/ElQuecus/FileChooseWindow/FileChooseModel.cs
+++ ElQuecus/ElQuecus/FileChooseWindow/FileChooseModel.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ElQuecus.Models;
 
 namespace ElQuecus.FileChooseWindow
 {
     internal class FileChooseModel
     {
-        public static void Method(string path)
+        public static async Task<List<Song>> GetSongList(string path)
         {
-            
+            return await ToTagFile.GetFiles(path);
         } 
     }
 }
